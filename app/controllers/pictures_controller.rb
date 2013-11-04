ef7b2892a509ede9c95cf85e2664d0e31f7b6d5a
rail v- <fgodoy2014@kellogg.northwestern.edu>
2013-11-04 15:55:31 -0600

    Almost there

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,32 @@
 class PicturesController < ApplicationController
   def show
+  	@pic = Picture.find(params[:id])
+  end
+  def index
+  	@pic = Picture.all
+  end
+  def new
+
+  end
+  def create
+  	p = Picture.new
+  	p.source = params[:source]
+  	p.caption = params[:caption]
+  	p.save
+  end
+  def destroy
+  	p = Picture.find(params[:id])
+  	p.delete
+
+  end
+  def edit
+  	@pic = Picture.find(params[:id])
+  end
+
+    def update
+  	p = Picture.find(params[:id])
+  	p.source = params[:source]
+  	p.caption = params[:caption]
+  	p.save
   end
 end
