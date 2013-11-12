e7830bb767bf048d25e3bced3232e3f0c2197082
rail v- <fgodoy2014@kellogg.northwestern.edu>
2013-11-11 19:16:49 -0600

    Restful links full

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rxngif::Application.routes.draw do
   
 
 # CREATE
-get '/pictures/new', :controller => 'pictures', :action => 'new', :as => 'new_post'
+get '/pictures/new', :controller => 'pictures', :action => 'new', :as => 'new_picture'
 post '/pictures', :controller => 'pictures', :action => 'create', :as => 'pictures'
 
 # READ
@@ -10,7 +10,7 @@ get '/pictures', :controller => 'pictures', :action => 'index'
 get '/pictures/:id', :controller => 'pictures', :action => 'show', :as => 'picture'
 
 # UPDATE
-get '/pictures/:id/edit', :controller => 'pictures', :action => 'edit', :as => 'edit_post'
+get '/pictures/:id/edit', :controller => 'pictures', :action => 'edit', :as => 'edit_picture'
 patch '/pictures/:id', :controller => 'pictures', :action => 'update'
 
 # DELETE
